c726009279e009a4f12fa741e83bb5797ec3fdc0
masood <masoodpk555@gmail.com>
2024-01-16 10:52:49 +0530

    input added

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
 
 import './App.css';
 import { useState } from 'react';
-
+function App(){
+  const [todos,setTodos]=useState([])
+  const [toDo,setToDo]=useState('')
+  return (
+    <div className="app">
+      <div className="mainHeading">
+        <h1>ToDo List</h1>
+      </div>
+      <div className="subHeading">
+        <br />
+        <h2>Whoop, it's Wednesday 🌝 ☕ </h2>
+      </div>
+      <div className="input">
+        <input value={toDos} onChange={(e)=>setToDos([
+          e.target.value
+        ])} type="text" placeholder="🖊️ Add item..." />
+        <i onClick={()=>setToDo(
+          [...toDo,{id:Date.now(),text:toDos,status:false}]
+        )} className="fas fa-plus"></i>
+      </div>
+      </div>
+  )
+}
 export default App;
