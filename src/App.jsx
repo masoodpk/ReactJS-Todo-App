6a238a57c91a7c7d4e952fa7b8a9483941a34d7d
masood <masoodpk555@gmail.com>
2024-01-16 11:04:30 +0530

    Todo .map items

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,49 @@ function App(){
           [...toDo,{id:Date.now(),text:toDos,status:false}]
         )} className="fas fa-plus"></i>
       </div>
+      <div className="todos">
+       {
+        toDo.map((obj)=>{
+          return(
+            <div className="todo">
+            <div className="left">
+              <input value={obj.status} onChange={(e)=>{
+                console.log(e.target.checked)
+                console.log(obj)
+                setToDo(toDo.filter(obj2=>{
+                  if(obj2.id===obj.id){
+                    obj2.status=e.target.checked
+                  }
+                  return obj2
+                }))
+                  
+                
+              }} type="checkbox" name="" id="" />
+              <p>{obj.text}</p>
+            </div>
+            <div className="right">
+              <i className="fas fa-times"></i>
+            </div>
+          </div>
+          )
+        }
+        )
+       }
+
+        {
+          toDo.map((obj) => {
+            if (obj.status) {
+              return (
+                <h1>{obj.text}</h1>
+              )
+            }
+            return null
+        })
+       }
+          
       </div>
+      </div>
+      
   )
 }
 export default App;
